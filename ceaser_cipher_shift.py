7082bc75498d032c71e17fe3dbbcc393516b297e
Precious <Kyu@users.noreply.github.com>
2023-09-09 12:56:13 -0500

    Add ceaser cipher program

---
+++ ceaser_cipher_shift.py
@@ -0,0 +1,35 @@
+import sys
+
+input_str = " ".join(sys.argv[1:])
+
+if input_str:
+    input_str = input("Enter the cipher string: ")
+
+
+def ceaser_shift(input_str: str, shift):
+    if shift == 0:
+        return input_str
+
+    result = ""
+    for i in input_str:
+        if not i.isalpha():
+            result += i
+            continue
+
+        is_upper = i.isupper()
+        base = 65 if is_upper else 97
+        new_chr = ord(i) - base + shift
+        new_chr %= 26
+
+        # print(f"{i}: {base}, {new_chr}")
+
+        result += chr(new_chr + base)
+
+    return result
+
+
+for i in range(1, 26):
+    print(i)
+
+print(input_str)
+print(ceaser_shift(input_str, 13))
